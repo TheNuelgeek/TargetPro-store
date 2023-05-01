ab39a25150bdd5ce2d1527ef5c44517f4f904370
TheNuelgeek <thenuelgeekmain@gmail.com>
2023-05-01 23:56:25 +0100

    still working on hero page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,9 +7,16 @@ function HeroSection(){
             <div className="hero-info">
                 <div className="hero-content">
                     <h3>03/03</h3>
+                    <div className="hero-content-down">
+                        
                     <h5>PUT ON TARGET PRO AND STEP OFF</h5>
                     <h1>TARGET PRO WEARS</h1>
                     <p>Quiet Luxury, the real luxury</p>
+                    </div>
+                    <div className="hero-content-btn">
+                        <button>SHOP MEN</button>
+                        <button>SHOP WOMEN</button>
+                    </div>
                 </div>
                 <div className="hero-img">
                 <img src={image1} alt="" className=""/>
